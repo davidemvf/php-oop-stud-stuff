d4041a2e7c97e4a61910183858368a3d831ddda7
davidemvf <davidemv.fiore@gmail.com>
2019-09-23 22:00:43 +0200

    Visualizzato Studente

--- obj.php
+++ obj.php
@@ -16,13 +16,28 @@ Class Persona {
 
     return '<br>' . 'Nome: ' . $this -> nome . '<br>' . 'Cognome: ' . $this -> cognome . '<br>' . 'Indirizzo: ' . $this -> indirizzo;
   }
+};
 
+class Studente extends Persona {
+  public $pianoStudi;
+  public $tasse;
 
+  public function __construct($nome, $cognome, $indirizzo, $pianoStudi, $tasse) {
 
+    parent::__construct($nome, $cognome, $indirizzo);
+    $this -> pianoStudi = $pianoStudi;
+    $this -> tasse = $tasse;
+  }
+
+  public function toString() {
+    return  parent::toString() . '<br>' . 'Piano di Studi: ' . $this -> pianoStudi . '<br>' . 'Tasse: ' . $this -> tasse;
+  }
 };
 
 $persona1 = new Persona ("Davide", "Fiore", "Via dei Matti");
+$studente1 = new Studente ("Mario", "Rossi", "Via dei Matti", "Energetico", "Pagate");
 
-echo $persona1 -> toString();
+echo "<br> Persona: <br>" . $persona1 -> toString() . '<br><br>';
+echo "Studente: <br>" . $studente1 -> toString() . '<br><br>';
 
 ?>
